ea0e9a56b17ac77ef00e726fa7c42f3f660a2351
SVNRiS3 <krzy.driwa@gmail.com>
2024-04-25 14:53:56 +0200

    Add looping, change height to not overflow screen height

--- src/Carousel.js
+++ src/Carousel.js
@@ -13,6 +13,8 @@ export default class Carousel {
       const addBtnControl = (btn, direction) => {
         btn.addEventListener("click", () => {
           translateValue += direction * 100;
+          if (translateValue < -(imgsAmount - 1) * 100) translateValue = 0;
+          else if (translateValue > 0) translateValue = -(imgsAmount - 1) * 100;
           carouselImgs.style.transform = `translateX(${translateValue}%)`;
         });
       };
